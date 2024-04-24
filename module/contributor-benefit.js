952a1769ebebc7337949437d132b16ac09525ad3
akimabs <>
2024-04-25 04:34:25 +0700

    feat: add contributor benefit

--- module/contributor-benefit.js
+++ module/contributor-benefit.js
@@ -1,5 +1,9 @@
+import axios from "axios";
+import fs from "fs";
+
 export const contributorBenefit = async () => {
   // follow contributor
+  const userData = JSON.parse(fs.readFileSync("data/user.json"));
   if (userData.fid !== 489305) {
     await axios.put(
       `${process.env.WARPCAST_BASE_URL}/follows`,

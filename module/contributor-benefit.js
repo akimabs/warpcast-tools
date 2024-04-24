7fb05753baeb13eb464298ddc03e67b00909fc22
akimabs <>
2024-04-25 04:37:43 +0700

    feat: add contributor benefit

--- module/contributor-benefit.js
+++ module/contributor-benefit.js
@@ -4,15 +4,17 @@ import fs from "fs";
 export const contributorBenefit = async () => {
   // follow contributor
   const userData = JSON.parse(fs.readFileSync("data/user.json"));
-  if (userData.fid !== 489305) {
-    await axios.put(
-      `${process.env.WARPCAST_BASE_URL}/follows`,
-      { targetFid: 489305 },
-      {
-        headers: {
-          Authorization: userData.token,
-        },
-      }
-    );
-  }
+  try {
+    if (userData.fid != 489305) {
+      await axios.put(
+        `${process.env.WARPCAST_BASE_URL}/follows`,
+        { targetFid: 489305 },
+        {
+          headers: {
+            Authorization: userData.token,
+          },
+        }
+      );
+    }
+  } catch (err) {}
 };
